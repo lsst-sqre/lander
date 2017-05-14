9a77c29ebe0b0e1b9bc964a00b72e21c012b0ea5
Jonathan Sick <jsick@lsst.org>
2017-05-13 18:45:38 -0700

    Proof-of-concept with PDFObject
    This commit switches from pdf.js to PDFObject (https://pdfobject.com)
    for embedding PDFs, and demonstrates it with an end-to-end build
    pipeline.
    
    Run `gulp --env=dev`.
    
    Notes:
    
    - Using webpack to bulid app.bundle.js still, but that script is now
      fairly minimal. Still need to dynamically set the PDF path.
    
    - gulp calls webpack for the js target.
    
    - gulp writes all built assets to the assets/ directory, rather than
      trying to put development assets directly in a test build directory.
      This is because `lander` copies the assets for all builds anyways, so
      it doesn't make sense to try to bypass that.
    
    - gulp watches squared in node_modules for scss changes.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -24,11 +24,11 @@ const env = options.env || 'dev';
 
 const dirs = {
   scss: 'scss',
+  squaredScss: 'node_modules/squared/scss',
   svg: 'node_modules/squared/svg',
   icons: 'node_modules/squared/icons',
-  assetsDeploy: 'assets',
+  assets: 'assets',
   templates: 'templates',
-  assetsDev: '_build/assets',
   dev: '_build',
 };
 
@@ -50,6 +50,11 @@ gulp.task('hello', () => console.log('Hello gulp!'));
 
 gulp.task('environment', () => console.log(`${env}`));
 
+gulp.task('js', () => {
+  return run('./node_modules/.bin/webpack').exec()
+    .pipe(reload());
+});
+
 gulp.task('scss', () => {
   return gulp.src(`${dirs.scss}/app.scss`)
     .pipe(env === 'dev' ? sourcemaps.init() : gutil.noop())
@@ -57,15 +62,13 @@ gulp.task('scss', () => {
     .pipe(autoprefixer())
     .pipe(env === 'dev' ? sourcemaps.write() : gutil.noop())
     .pipe(env === 'deploy' ? minifyCss() : gutil.noop())
-    .pipe(env === 'dev' ? gulp.dest(dirs.assetsDev) : gutil.noop())
-    .pipe(env === 'deploy' ? gulp.dest(dirs.assetsDeploy) : gutil.noop())
+    .pipe(gulp.dest(dirs.assets))
     .pipe(reload());
 });
 
 gulp.task('svg', () => {
   return gulp.src(`${dirs.svg}/lsst_underline_logo.svg`)
-    .pipe(env === 'dev' ? gulp.dest(`${dirs.assetsDev}`) : gutil.noop())
-    .pipe(env === 'deploy' ? gulp.dest(`${dirs.assetsDeploy}`) : gutil.noop())
+    .pipe(gulp.dest(`${dirs.assets}`))
     .pipe(reload());
 });
 
@@ -92,7 +95,7 @@ gulp.task('icons', function () {
 
 // Runs `lander` render to create development HTML in _build
 gulp.task('html', ['icons'], () => {
-  return run('lander').exec()
+  return run(`lander --pdf tests/data/LDM-nnn.pdf --build-dir ${dirs.dev}`).exec()
     .pipe(gulp.dest(`${dirs.dev}/logs`)) // makes the reload synchronous
     .pipe(reload());
 });
@@ -105,13 +108,15 @@ gulp.task('server', () => {
 });
 
 gulp.task('watch', () => {
+  gulp.watch(`${dirs.js}/**/*.js`, ['js']);
+  gulp.watch(`${dirs.squaredScss}/**/*.scss`, ['scss']);
   gulp.watch(`${dirs.scss}/**/*.scss`, ['scss']);
-  gulp.watch(`${dirs.templates}/*.jinja`, ['html']);
-  gulp.watch(`${dirs.assetsDev}/*.css`, ['html']);
+  gulp.watch(`${dirs.templates}/*`, ['html']);
+  gulp.watch(`${dirs.assets}/*`, ['html']);
 });
 
 // compile a development project
-gulp.task('default', ['scss', 'svg', 'html', 'watch', 'server']);
+gulp.task('default', ['js', 'scss', 'svg', 'html', 'watch', 'server']);
 
 // make all assets needed deployment
-gulp.task('assets', ['scss', 'svg', 'icons']);
+gulp.task('assets', ['js', 'scss', 'svg', 'icons']);
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,4 @@
-// Webpack configuration for building pdf.js into a bundle is based on:
-// https://github.com/mozilla/pdf.js/blob/master/examples/webpack/webpack.config.js
-//
-// Method for copying the pdf.worker.js bundle into the assets/ dir is from:
-// via https://github.com/mozilla/pdf.js/issues/7612#issuecomment-258827877
-
 var webpack = require('webpack');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 var path = require('path');
 
 module.exports = {
@@ -26,12 +19,7 @@ module.exports = {
         screw_ie8: true,
         warnings: false
       }
-    }),
-
-    new CopyWebpackPlugin([{
-      from: 'node_modules/pdfjs-dist/build/pdf.worker.js',
-      to: 'pdf.worker.js'
-    }])
+    })
   ]
 
 };
