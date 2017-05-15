1dbc1e077c21ce2ba81ac59dea8478420bae2018
Jonathan Sick <jsick@lsst.org>
2017-05-14 22:13:42 -0700

    Basic HTML page layout
    - universal header across top to link back to lsst.org
    - metadata in a left column. Include title/handle components based on
      the https://github.com/lsst-sqre/ltd-dasher project's dashboards.
    - PDFObject in the right column
    
    I'm using flexbox to layout (in row mode) for the metadata and pdf
    columns. This is far easier than managing floats, particularly since the
    PDF expands to fit the container, and that was interfering with the
    floats.
    
    NOTE: This is still a proof-of-concept. Test metadata is still being
    shimmed into the config.

--- lander/config.py
+++ lander/config.py
@@ -2,6 +2,7 @@
 """
 import sys
 import os
+import re
 
 import structlog
 
@@ -26,8 +27,12 @@ class Configuration(object):
             sys.exit(1)
 
         # Defaults for testing
-        self._config['title'] = 'Title'
-        self._config['abstract'] = 'Abstract'
+        self['title'] = 'Title'
+        self['abstract'] = 'Abstract'
+        self['doc_handle'] = 'LDM-000'
+
+        self['series'] = self._get_series(self['doc_handle'])
+        self['series_name'] = self._get_series_name(self['series'])
 
     def __getitem__(self, key):
         """Access configurations, first from the explicitly set configurations,
@@ -40,3 +45,20 @@ class Configuration(object):
 
     def __setitem__(self, key, value):
         self._config[key] = value
+
+    def _get_series(self, doc_handle):
+        pattern = re.compile('^(sqr|dmtn|smtn|ldm|lse|lpm)-[0-9]+$')
+        print(doc_handle)
+        match = pattern.search(doc_handle.lower())
+        return match.group(1).upper()
+
+    def _get_series_name(self, series):
+        series_names = {
+            'sqr': 'SQuaRE Technical Note',
+            'dmtn': 'Data Management Technical Note',
+            'smtn': 'Simulations Technical Note',
+            'ldm': 'LSST Data Management',
+            'lse': 'LSST Systems Engineering',
+            'lpm': 'LSST Project Management',
+        }
+        return series_names.get(series.lower(), '')
