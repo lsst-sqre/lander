db8cabf8698bb72c24afe60f66af097aa4b61203
Jonathan Sick <jsick@lsst.org>
2017-05-18 16:09:57 -0700

    Add build date to the document info header
    - Add the datetime of the PDF build.
    - Use the calendar octicon.
    - Integrate it with the version display/switcher through a <ul> below
      the header.
    
    Adds pytz as a dependency in order to get a proper UTC date.
    
    Now that we're just using the current time of the build. This works in a
    CI continuous delivery mode, but doesn't work for old docs being
    deployed now. Ultimately we should get the date from the last git commit
    of fits in the tex document tree.

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
     keywords='lsst',
     packages=find_packages(exclude=['docs', 'tests*', 'data']),
     install_requires=[
+        'pytz',
         'Jinja2==2.9.6',
         'structlog==17.1.0',
         'ltd-conveyor==0.3.1',
