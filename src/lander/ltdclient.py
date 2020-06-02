6472829d70ea108ec90d7295084d5c0778b489e3
Jonathan Sick <jsick@lsst.org>
2020-06-02 18:38:25 -0400

    Adopt presigned-post based LTD Conveyor APIs
    This enables upload to LSST the Docs without knowing any AWS
    credentials. LTD Keeper generates presigned URLs for you.

--- src/lander/ltdclient.py
+++ src/lander/ltdclient.py
@@ -1,11 +1,18 @@
 """Client for uploading to LSST the Docs.
 """
 
-import ltdconveyor
+from pathlib import Path
+from typing import TYPE_CHECKING, Any, Dict
+
 import requests
+from ltdconveyor.keeper import confirm_build, get_keeper_token, register_build
+from ltdconveyor.s3.presignedpost import prescan_directory, upload_dir
+
+if TYPE_CHECKING:
+    from lander.config import Configuration
 
 
-def upload(config):
+def upload(config: "Configuration") -> None:
     """Upload the build documentation site to LSST the Docs.
 
     Parameters
@@ -16,69 +23,27 @@ def upload(config):
     token = get_keeper_token(
         config["keeper_url"], config["keeper_user"], config["keeper_password"]
     )
-    build_resource = register_build(config, token)
-
-    ltdconveyor.upload_dir(
-        build_resource["bucket_name"],
-        build_resource["bucket_root_dir"],
-        config["build_dir"],
-        aws_access_key_id=config["aws_id"],
-        aws_secret_access_key=config["aws_secret"],
-        surrogate_key=build_resource["surrogate_key"],
-        cache_control="max-age=31536000",
-        surrogate_control=None,
-        upload_dir_redirect_objects=True,
+    dirnames = prescan_directory(config.build_dir)
+    build_resource = register_build(
+        config.ltd_url,
+        token,
+        config.ltd_product,
+        git_refs=[config.github_ref],
+        dirnames=dirnames,
     )
 
-    confirm_build(config, token, build_resource)
-
-
-def get_keeper_token(base_url, username, password):
-    """Get a temporary auth token from LTD Keeper."""
-    token_endpoint = base_url + "/token"
-    r = requests.get(token_endpoint, auth=(username, password))
-    if r.status_code != 200:
-        raise RuntimeError(
-            "Could not authenticate to {0}: error {1:d}\n{2}".format(
-                base_url, r.status_code, r.json()
-            )
-        )
-    return r.json()["token"]
-
-
-def register_build(config, keeper_token):
-    data = {
-        "git_refs": [config["git_branch"]],
-    }
-
-    r = requests.post(
-        config["keeper_url"]
-        + "/products/{p}/builds/".format(p=config["ltd_product"]),
-        auth=(keeper_token, ""),
-        json=data,
+    upload_dir(
+        post_urls=build_resource["post_prefix_urls"],
+        base_dir=Path(config.build_dir),
     )
 
-    if r.status_code != 201:
-        raise RuntimeError(r.json())
-    build_info = r.json()
-    return build_info
+    confirm_build(build_resource["self_url"], token)
 
 
-def confirm_build(config, keeper_token, build_resource):
-    build_url = build_resource["self_url"]
-    r = requests.patch(
-        build_url, auth=(keeper_token, ""), json={"uploaded": True}
-    )
-    if r.status_code != 200:
-        raise RuntimeError(r)
-
-
-def get_product(config):
+def get_product(config: "Configuration") -> Dict[str, Any]:
     """Get the /product/<product> resource from LTD Keeper.
     """
-    product_url = config["keeper_url"] + "/products/{p}".format(
-        p=config["ltd_product"]
-    )
+    product_url = config.ltd_url + "/products/{p}".format(p=config.ltd_product)
     r = requests.get(product_url)
     if r.status_code != 200:
         raise RuntimeError(r.json())
