e12a8a8d34adc049a135701f5c4b5fc0ce39d9c5
Jonathan Sick <jsick@lsst.org>
2020-12-15 17:45:10 -0500

    Convert Person.orcid to Orcid type
    This allows us to validate the ORCiD and ensure that the ORCiD is always
    a URL, as specified by
    https://support.orcid.org/hc/en-us/articles/360006897674
    
    Validation is implemented by subclassing the HttpUrl pydantic type so
    that after the ORCiD identifier is parsed, it becomes a URL type.

--- src/lander/ext/parser/_datamodel.py
+++ src/lander/ext/parser/_datamodel.py
@@ -2,17 +2,40 @@ from __future__ import annotations
 
 import datetime
 import re
-from typing import List, Optional
+from typing import TYPE_CHECKING, Any, Generator, List, Optional
 
 import bleach
-from pydantic import BaseModel, EmailStr, Field, HttpUrl, validator
+from pydantic import BaseConfig, BaseModel, EmailStr, Field, HttpUrl, validator
+from pydantic.errors import UrlError
+from pydantic.fields import ModelField
 
 from lander.ext.parser.pandoc import convert_text
 from lander.spdx import Licenses
 
-__all__ = ["FormattedString", "Person", "DocumentMetadata"]
+if TYPE_CHECKING:
+    from pydantic.typing import AnyCallable
+
+    CallableGenerator = Generator[AnyCallable, None, None]
+
+
+__all__ = ["FormattedString", "Person", "Orcid", "DocumentMetadata"]
 
 WHITESPACE_PATTERN = re.compile(r"\s+")
+
+ORCID_PATTERN = re.compile(
+    r"(?P<identifier>[0-9X]{4}-[0-9X]{4}-[0-9X]{4}-[0-9X]{4})"
+)
+"""Regular expression for matching the ORCiD identifier
+
+Examples:
+
+- 0000-0002-1825-0097
+- 0000-0001-5109-3700
+- 0000-0002-1694-233X
+
+For more information, see
+https://support.orcid.org/hc/en-us/articles/360006897674
+"""
 
 
 def collapse_whitespace(text: str) -> str:
@@ -91,13 +114,68 @@ class FormattedString(BaseModel):
         return collapse_whitespace(v)
 
 
+class OrcidError(UrlError):
+    code = "orcid"
+    msg_template = "invalid ORCiD"
+
+
+class Orcid(HttpUrl):
+    """An ORCiD type for Pydantic validation.
+
+    The validator forces an ORCiD identifier to always be a URL for
+    orcid.org, per https://support.orcid.org/hc/en-us/articles/360006897674.
+    This validator implments the ISO 7064 11,2 checksum algorithm.
+    """
+
+    allowed_schemes = {"https"}
+
+    @classmethod
+    def __get_validators__(cls) -> "CallableGenerator":
+        yield cls.validate
+
+    @classmethod
+    def validate(
+        cls, value: Any, field: ModelField, config: BaseConfig
+    ) -> Orcid:
+        if value.__class__ == cls:
+            return value
+
+        m = ORCID_PATTERN.search(value)
+        if not m:
+            raise OrcidError()
+
+        identifier = m["identifier"]
+        if not cls.verify_checksum(identifier):
+            raise OrcidError()
+
+        return HttpUrl.validate(
+            f"https://orcid.org/{identifier}", field, config
+        )
+
+    @staticmethod
+    def verify_checksum(identifier: str) -> bool:
+        """Verify the checksum of an ORCiD identifier string (path component
+        of the URL) given teh ISO 7064 11,2 algorithm.
+        """
+        total: int = 0
+        for digit in identifier:
+            if digit == "X":
+                digit = "10"
+            if not digit.isdigit():
+                continue
+            total = (total + int(digit)) * 2
+        remainder = total % 11
+        result = (12 - remainder) % 11
+        return result == 10
+
+
 class Person(BaseModel):
     """Data about a person."""
 
     name: str
     """Display name of the person."""
 
-    orcid: Optional[str] = None
+    orcid: Optional[Orcid] = None
     """The ORCiD of the person."""
 
     affiliations: Optional[List[str]] = Field(default_factory=lambda: [])
--- tests/ext_parser_datamodel_test.py
+++ tests/ext_parser_datamodel_test.py
@@ -3,8 +3,13 @@
 from __future__ import annotations
 
 import pytest
+from pydantic import BaseModel, ValidationError
 
-from lander.ext.parser._datamodel import FormattedString, collapse_whitespace
+from lander.ext.parser._datamodel import (
+    FormattedString,
+    Orcid,
+    collapse_whitespace,
+)
 
 
 def test_formattedstring_sanitize_html() -> None:
@@ -40,3 +45,37 @@ def test_formattedstring_from_latex_paragraph() -> None:
 )
 def test_collapse_whitespace(text: str, expected: str) -> None:
     assert collapse_whitespace(text) == expected
+
+
+@pytest.mark.parametrize(
+    "identifier,sample",
+    [
+        ("0000-0002-1825-0097", "0000-0002-1825-0097"),
+        ("0000-0001-5109-3700", "hello 0000-0001-5109-3700 world"),
+        ("0000-0002-1694-233X", "0000-0002-1694-233X"),
+        ("0000-0002-1825-0097", "https://orcid.org/0000-0002-1825-0097"),
+        ("0000-0001-5109-3700", "http://0000-0001-5109-3700"),
+        ("0000-0002-1694-233X", "https://0000-0002-1694-233X"),
+    ],
+)
+def test_orcid(identifier: str, sample: str) -> None:
+    class Model(BaseModel):
+        orcid: Orcid
+
+    m = Model(orcid=sample)
+
+    assert m.orcid == f"https://orcid.org/{identifier}"
+    assert m.orcid.path == f"/{identifier}"
+    assert m.orcid.host == "orcid.org"
+    assert m.orcid.scheme == "https"
+
+
+@pytest.mark.parametrize("sample", ["0000-0002-1825-0099", "0001-5109-3700"])
+def test_orcid_fail(sample: str) -> None:
+    """Test mal-formed ORCiD (wrong checksum or wrong pattern)."""
+
+    class Model(BaseModel):
+        orcid: Orcid
+
+    with pytest.raises(ValidationError):
+        Model(orcid=sample)
