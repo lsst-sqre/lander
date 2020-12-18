27c6a8cd3d9d13c7998dda08ff80f4da62daeec6
Jonathan Sick <jsick@lsst.org>
2020-12-18 17:26:18 -0500

    Parse date command in LaTeX articles

--- src/lander/parsers/article/_parser.py
+++ src/lander/parsers/article/_parser.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from typing import Optional
+
 from lander.ext.parser import DocumentMetadata, Parser
 from lander.ext.parser.texutils.extract import (
     LaTeXCommand,
@@ -25,7 +27,14 @@ class ArticleParser(Parser):
         metadata
             The metadata parsed from the document source.
         """
-        return DocumentMetadata(title=self._parse_title(tex_source),)
+        try:
+            date_modified: Optional[str] = self._parse_date(tex_source)
+        except RuntimeError:
+            date_modified = None
+
+        return DocumentMetadata(
+            title=self._parse_title(tex_source), date_modified=date_modified
+        )
 
     def _parse_title(self, tex_source: str) -> str:
         command = LaTeXCommand(
@@ -39,3 +48,13 @@ class ArticleParser(Parser):
         if len(titles) == 0:
             raise RuntimeError("Could not parse a title command.")
         return titles[-1]["long_title"]
+
+    def _parse_date(self, tex_source: str) -> str:
+        command = LaTeXCommand(
+            "date",
+            LaTeXCommandElement(name="date", required=True, bracket="{"),
+        )
+        dates = [t for t in command.parse(tex_source)]
+        if len(dates) == 0:
+            raise RuntimeError("Could not parse a date command.")
+        return dates[-1]["date"]
